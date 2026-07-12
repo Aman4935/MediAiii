49604f803d8a26f9d3a12905a378342ede87ffda
Aman4935 <ubs4935@gmail.com>
2026-07-12 23:28:53 +0530

    Update frontend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "https://mediaiii-backend.onrender.com/api",
 });
+
+export default api;
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
