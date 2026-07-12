eebcd4a90ff192673f2ef290ae587e1b04981077
Aman4935 <ubs4935@gmail.com>
2026-07-13 00:30:04 +0530

    Fix SMTP

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -2,15 +2,12 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
 });
 
 const sendMail = async ({ to, subject, html }) => {
