262db354abf25b9934b791829ff666ef3e2f3dab
Aman4935 <ubs4935@gmail.com>
2026-07-12 23:35:20 +0530

    Updated API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,16 +4,18 @@ const api = axios.create({
   baseURL: "https://mediaiii-backend.onrender.com/api",
 });
 
-export default api;
+// Add JWT token automatically
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
